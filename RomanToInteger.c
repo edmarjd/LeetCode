183f347d2e3262807f59de7b41efd55ed83d4cf6
edmarjd <fedmar049@gmail.com>
2025-05-15 18:57:24 -0300

    RomanToInteger

---
+++ RomanToInteger.c
@@ -0,0 +1,33 @@
+/*
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+*/
+
+
+int romanToInt(char *s) {
+    int values[256] = {0};
+    values['I'] = 1;
+    values['V'] = 5;
+    values['X'] = 10;
+    values['L'] = 50;
+    values['C'] = 100;
+    values['D'] = 500;
+    values['M'] = 1000;
+
+    int sum = 0;
+    int length = strlen(s);
+
+    for (int i = 0; i < length; i++) {
+        int current = values[s[i]];
+        if (i + 1 < length) {
+            int next = values[s[i + 1]];
+            if (current < next) {
+                sum += (next - current);
+                i++;
+                continue;
+            }
+        }
+        sum += current;
+    }
+
+    return sum;
+}
