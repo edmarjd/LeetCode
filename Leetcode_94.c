114d3522a0834a412aedb16a8175de6a842fee0f
edmarjd <fedmar049@gmail.com>
2025-08-03 13:30:12 -0300

    Update Leetcode_94.c

--- Leetcode_94.c
+++ Leetcode_94.c
@@ -10,7 +10,6 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 
-<<<<<<< HEAD
 void inorder (struct TreeNode* root, int* result , int* index) {
     if (root == NULL) {
         return;
@@ -31,26 +30,3 @@ int* inorderTraversal(struct TreeNode* root, int* returnSize) {
 
     return result;
 }
-=======
-void inorder (struct TreeNode* root, int* result , int* index) {
-    if (root == NULL) {
-        return;
-    }
-
-    inorder(root->left , result, index);
-    result[(*index)++] = root->val;
-    inorder(root->right , result , index);
-}
-
-int* inorderTraversal(struct TreeNode* root, int* returnSize) {
-    
-    int *result = (int *) malloc(1000 * sizeof(int));
-    int index = 0;
-
-    inorder(root, result, &index);
-    *returnSize = index;
-
-    return result;
-}
-
->>>>>>> a0003d77640a23a69b34d7f1a187ca697f1c0a8d
